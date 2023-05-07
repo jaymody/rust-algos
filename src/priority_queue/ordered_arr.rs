82d1f2b3cc366d0ca4e9bb354e8f76dd7320ecac
jaymody <jaykmody@gmail.com>
2023-05-07 16:40:05 -0400

    Use binary search for PriorityQueueOrderedArr.

--- src/priority_queue/ordered_arr.rs
+++ src/priority_queue/ordered_arr.rs
@@ -4,11 +4,16 @@ sorted order (descending sorted) and a value that tracks the current number
 of items.
 
 Pushing to the queue requires O(n) time. We first find the position the item
-belongs and then we shift the rest of the items to make space for it.
+belongs (which we can do using binary search since the array is sorted) which is
+O(log n) time, but then we need to shift the rest of the items, which is in
+the worst case an O(n) operation (i.e. if the item needs to be inserted at the
+start of the array).
 
 Peeking and popping require O(1) time, since the array is sorted, we simply
 retrieve the last entry.
 */
+
+use crate::search::binary_search_insert_index_rev;
 
 use super::PriorityQueue;
 
@@ -34,12 +39,9 @@ impl<T: Ord, const CAPACITY: usize> PriorityQueue<T> for PriorityQueueOrderedArr
             return Err("capacity full".to_string());
         }
 
-        let mut i = 0;
-        while i < self.size && self.arr[i].as_ref().unwrap() > &item {
-            i += 1;
-        }
-
         let mut prev = Some(item);
+        let i = binary_search_insert_index_rev(&self.arr[..self.size], &prev);
+
         for j in i..=self.size {
             (self.arr[j], prev) = (prev, self.arr[j].take());
         }
