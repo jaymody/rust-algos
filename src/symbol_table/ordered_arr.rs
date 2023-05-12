c024e8bef47204c653cdc33395d2c1b26a507118
jaymody <jaykmody@gmail.com>
2023-05-12 12:21:26 -0400

    Refactor tests.

--- src/symbol_table/ordered_arr.rs
+++ src/symbol_table/ordered_arr.rs
@@ -132,3 +132,50 @@ impl<'a, K: KeyT, V, const CAPACITY: usize> IntoIterator for &'a OrderedArrST<K,
         IntoIter { st: self, i: 0 }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ordered_arr_st() {
+        let mut st: OrderedArrST<&str, i32, 5> = OrderedArrST::new();
+        assert_eq!(st.is_empty(), true);
+        assert_eq!(st.size(), 0);
+
+        st.put("apple", 10).unwrap();
+        st.put("banana", 5).unwrap();
+        st.put("cat", -3).unwrap();
+        st.put("dog", 0).unwrap();
+        assert_eq!(st.size(), 4);
+        assert_eq!(st.is_empty(), false);
+
+        assert_eq!(st.get("apple"), Some(&10));
+        assert_eq!(st.get("banana"), Some(&5));
+        assert_eq!(st.get("cat"), Some(&-3));
+        assert_eq!(st.get("dog"), Some(&0));
+        assert_eq!(st.get("elephant"), None);
+
+        assert_eq!(st.pop("cat"), Some(-3));
+        assert_eq!(st.get("cat"), None);
+        assert_eq!(st.pop("cat"), None);
+        assert_eq!(st.get("dog"), Some(&0));
+
+        st.put("apple", 20).unwrap();
+        assert_eq!(st.get("apple"), Some(&20));
+        assert_eq!(st.size(), 3);
+
+        assert!(st.put("a", 1).is_ok());
+        assert!(st.put("b", 1).is_ok());
+        assert!(st.put("a", 1).is_ok());
+        assert!(st.put("c", 1).is_err());
+
+        let mut iter = (&st).into_iter();
+        assert_eq!(iter.next(), Some(&"a"));
+        assert_eq!(iter.next(), Some(&"apple"));
+        assert_eq!(iter.next(), Some(&"b"));
+        assert_eq!(iter.next(), Some(&"banana"));
+        assert_eq!(iter.next(), Some(&"dog"));
+        assert_eq!(iter.next(), None);
+    }
+}
